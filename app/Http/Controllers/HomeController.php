c0719c5642b889e73c8dabe2536c2444fb5f5d19
{km} <{k3042227625n@gmail.com}>
2023-07-14 13:30:06 +0900

    リレーション機能

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,7 +31,7 @@ class HomeController extends Controller
         // ASC=昇順,DESC=降順
         $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
         // dd($memos);
-        return view('home', compact('user', 'memos'));
+        return view('create', compact('user', 'memos'));
     }
 
     public function create()
@@ -49,10 +49,17 @@ class HomeController extends Controller
         // dd($data);
         // POSTされたデータをDB（memosテーブル）に挿入
         // MEMOモデルにDBへ保存する命令を出す
-        
-        // 先にタグをインサート
-        $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
-        // dd($tag_id);
+
+        // 同じタグがあるか確認
+        $exist_tag = Tag::where('name', $data['tag'])->where('user_id', $data['user_id'])->first();
+        // dd($exist_tag);
+        if ( empty($exist_tag['id'])){
+            // 先にタグをインサート
+            $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
+            // dd($tag_id);
+        } else {
+            $tag_id = $exist_tag['id'];
+        }
 
         // タグのIDが判明する
         // タグのIDをmemosテーブルに入れてあげる
@@ -84,6 +91,17 @@ class HomeController extends Controller
         // dd($inputs);
         Memo::where('id', $id)->update(['content' => $inputs['content'], 'tag_id' => $inputs['tag_id'] ]);
         return redirect()->route('home');
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $inputs = $request->all();
+        // dd($inputs);
+        Memo::where('id', $id)->update(['status' => 2 ]);
+        // ⇩は論理削除
+        // Memo::where('id', $id)->delete();
+
+        return redirect()->route('home')->with('success', 'メモの削除が完了しました！');
     }
 
 }
